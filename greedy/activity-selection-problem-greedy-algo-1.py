5e272488647ca662da9368daeef62a37a6ad81c4
congyingTech <congyingTech@163.com>
2019-11-26 22:55:48 +0800

    max activity nums

---
+++ greedy/activity-selection-problem-greedy-algo-1.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+# 先对f进行从小到大的排序，然后i是start的下标，j是finish的下标，如果后一个start的时间大于等于前一个finish的时间
+# 那么后一个活动可以加入！
+
+def solveMaxActivities(s,f):
+    n = len(s)
+    print('activity 0 is selected!')
+    i = 0 # start的下标
+    for j in range(1, n):
+        if s[j]>=f[i]:
+            print('activity {} is selected!'.format(j))
+            i = j
+
+if __name__ == "__main__":
+    s = [1 , 3 , 0 , 5 , 8 , 5] 
+    f = [2 , 4 , 6 , 7 , 9 , 9] 
+    solveMaxActivities(s,f)
