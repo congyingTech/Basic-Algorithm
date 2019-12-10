12c6b63dc09b5cb7ef851959f7e143c714249ab4
congyingTech <congyingTech@163.com>
2019-12-10 15:17:59 +0800

    add new problem

---
+++ medium/all-paths-from-source-to-target.py
@@ -0,0 +1,16 @@
+#encoding:utf-8
+"""
+问题描述：
+解决方案：
+"""
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        pass
+if __name__ == "__main__":
+    s = Solution()
+    s.allPathsSourceTarget(graph)
+
