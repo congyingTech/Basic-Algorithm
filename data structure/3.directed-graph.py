0fe54964676b0a9dbb7fc434b1d275d69cd677a8
congyingTech <congyingTech@163.com>
2019-12-11 11:35:00 +0800

    add directed graph

---
+++ data structure/3.directed-graph.py
@@ -0,0 +1,13 @@
+# encoding:utf-8
+"""
+问题描述：有向图
+"""
+class DirectedGraph(object):
+    def __init__(self):
+        pass 
+
+    def add_edges(self):
+        pass
+
+if __name__ == "__main__":
+    pass
