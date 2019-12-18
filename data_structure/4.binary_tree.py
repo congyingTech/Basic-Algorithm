638c94695028a1b66c6512012056d75cca3ed6d7
congyingTech <congyingTech@163.com>
2019-12-18 19:29:45 +0800

    waiting full it

---
+++ data_structure/4.binary_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+    
+    def level_order_print(self):
+        # 二叉树的层序遍历
+        pass
+
+    def pre_order_print(self):
+        # 二叉树的前序遍历
+        pass
+
+    def in_order_print(self):
+        # 二叉树的中序遍历
+        pass
+
+    def af_order_print(self):
+        # 二叉树的后序遍历
+        pass
+
+if __name__ == "__main__":
+    pass 
