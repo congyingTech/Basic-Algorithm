968de4eb971ea70a7b56dc7c3f7ca804aa272a25
congyingTech <congyingTech@163.com>
2019-12-21 09:35:06 +0800

    add new problem

---
+++ medium/count-square-submatrices-with-all-ones.py
@@ -0,0 +1,11 @@
+#encoding:utf-8
+class Solution(object):
+    def countSquares(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+if __name__ == "__main__":
+    s = Solution()
+    matrix = []
+    s.countSquares(matrix)
