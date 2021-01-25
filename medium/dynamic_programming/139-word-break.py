5b34e2055047e8556467baadaac8a13f450e2b37
congyingTech <congyingTech@163.com>
2021-01-25 23:18:16 +0800

    add 139

--- medium/dynamic_programming/139-word-break.py
+++ medium/dynamic_programming/139-word-break.py
@@ -17,15 +17,34 @@
 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
      注意你可以重复使用字典中的单词。
 
+
+解题思路：
+动态规划：
+dp[i]表示第i位时是否可以拆分成功，第i位字母是否可以拆分成功，取决于，前i-1位组成的字符串是否可以拆分成功，
+前i-1位主要又包含从s[j:i-1]是否是dict中的一个单词 && s[0:j-1]是否可以拆分。
+所以递推公式是：
+
+dp[i] = dp[j] && check_in_dict[s[j:i-1]]
+
 """
 
 
 class Solution:
     def wordBreak(self, s, wordDict):
-        pass
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                check_s = s[j:i]
+                if dp[j] and check_s in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[len(s)]
 
 
 if __name__ == "__main__":
     s = Solution()
-    st = ''
-    s.wordBreak()
+    st = "leetcode"
+    wordDict = ["leet", "code"]
+    print(s.wordBreak(st, wordDict))
