995f9da8e06d1381ff7e3184cdadedca5cbf0833
congyingTech <wangcongyinga@gmail.com>
2021-02-02 19:56:10 +0800

    add 120

--- medium/dynamic_programming/120-triangle.py
+++ medium/dynamic_programming/120-triangle.py
@@ -38,12 +38,12 @@ class Solution(object):
             dp[i][0] = dp[i-1][0] + triangle[i][0]
             dp[i][i] = dp[i-1][i-1] + triangle[i][i]
 
-        for i in range(2, m):
-            for j in range(2, i+1):
-                if j >= 2 and i >= 2:
+        for i in range(2, m-1):
+            for j in range(1, i):
+                if j >= 1 and i >= 2:
                     dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
 
-        return dp[m-1][m-1]
+        return min(dp[m-2])
 
 
 if __name__ == "__main__":
