f44d84966789b5ab7fe25cd99326fba36c8ff2d2
congyingTech <wangcongyinga@gmail.com>
2021-02-04 20:04:02 +0800

    add 322

---
+++ medium/dynamic_programming/322-coin-change.py
@@ -0,0 +1,40 @@
+"""
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+
+你可以认为每种硬币的数量是无限的。
+
+ 
+
+示例 1：
+
+输入：coins = [1, 2, 5], amount = 11
+输出：3
+解释：11 = 5 + 5 + 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/coin-change
+
+解题思路：
+xi是面值为ci的硬币的数量，i是面值的下标
+min(∑x(i))
+约束条件：x(i)*c(i) <= S
+dp[S]表示组成S金额的最少的硬币数
+dp[S] = dp[S-c(i)] + 1 0<=i<3
+"""
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    coins = []
+    amount = 0
+    s.coinChange(coins, amount)
