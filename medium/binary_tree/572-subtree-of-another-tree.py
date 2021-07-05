f474e880517ccc50803caf921c485c5a368efd72
congyingTech <congyingTech@163.com>
2021-07-06 01:42:02 +0800

    fix

--- medium/binary_tree/572-subtree-of-another-tree.py
+++ medium/binary_tree/572-subtree-of-another-tree.py
@@ -9,6 +9,10 @@ s=   3
 t= 4 
   / \
  1   2
+
+给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+
+
 解决方案：
 辅助函数isMatch是判断两个树结构是否完全一致，可以判断s和t是否完全一致，
 判断的条件是: s.val == t.val and self.isMatch(s.left, t.left) and self.isMatch(s.right, t.right)
@@ -16,11 +20,14 @@ t= 4
 """
 # Definition for a binary binary_tree node.
 
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
+
 # 递归的方法
 class Solution(object):
     def isSubtree(self, s, t):
