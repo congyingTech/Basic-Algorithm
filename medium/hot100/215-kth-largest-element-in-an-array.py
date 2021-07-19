dfd0c358713cd1a641732d1ad08c00074204202e
congyingTech <wangcongyinga@gmail.com>
2021-07-19 20:24:33 +0800

    215 undo

---
+++ medium/hot100/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/12 7:44 下午
+# @Author  : Mohn
+# @FileName: 215-kth-largest-element-in-an-array.py
+"""
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+ 
+
+示例 1:
+
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+
+
+if __name__ == "__main__":
+
+    print(Solution().findKthLargest([3, 2, 1, 5, 6, 4], 2))
