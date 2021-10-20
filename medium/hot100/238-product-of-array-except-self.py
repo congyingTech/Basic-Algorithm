cdfb2d545e8357ead1d0a287a098e84012713764
congyingTech <wangcongyinga@gmail.com>
2021-10-20 20:48:11 +0800

    add 238

--- medium/hot100/238-product-of-array-except-self.py
+++ medium/hot100/238-product-of-array-except-self.py
@@ -30,8 +30,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        L = [1]
-        R = [1]
+        n = len(nums)
+        L = [0] * n
+        R = [0] * n
+        answer = [0] * n
+        L[0] = 1
+        R[n-1] = 1
+        for i in range(1, n):
+            L[i] = L[i-1]*nums[i-1]
+        for i in range(n-2, -1, -1):
+            R[i] = R[i+1] * nums[i+1]
+
+        for i in range(n):
+            answer[i] = L[i] * R[i]
+        return answer
 
 
 if __name__ == "__main__":
